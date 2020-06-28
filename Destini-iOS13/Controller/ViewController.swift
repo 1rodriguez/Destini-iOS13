c454f341be1ae287577314e109e29e5fd273496e
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-28 11:33:35 -0400

    Pre-MVC

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,20 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road"
-    let choice1 = "Take a left "
-    let choice2 = "Take a right"
+    var track = 0 // Number to keep track of story progress
+    
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left", choice2: "Take a right"), Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"), Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        storyLabel.text! = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
 
+        updateUI(track)
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.titleLabel!.text! == choice1Button.titleLabel!.text! {
+            track = 1
+        } else { // Choice 2 was made
+            track = 2
+        }
+        updateUI(track)
+    }
+    
+    func updateUI(_ storyNum: Int) {
+        storyLabel.text! = stories[storyNum].storyTitle
+        choice1Button.setTitle(stories[storyNum].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNum].choice2, for: .normal)
     }
     
 }
